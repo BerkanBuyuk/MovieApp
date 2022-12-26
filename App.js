adb20210be127c11bb9e0b19b99f85ca70d2831b
Berkan Buyuk <berkanbyk55@gmail.com>
2022-12-26 15:20:01 +0300

    Header and icons done.

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Constants from './src/Constants';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>Deneme</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Stack.Screen name="Home" component={Home} options={headerStyle} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+const headerStyle = {
+  title: 'Movies',
+  headerStyle: {backgroundColor: Constants.baseColor},
+  headerTitleStyle: {color: Constants.textColor},
+  headerLeft: () => <Icon name="menu" size={34} color={Constants.textColor} />,
+  headerRight: () => (
+    <Icon name="search" size={25} color={Constants.textColor} />
+  ),
 };
 
 export default App;
